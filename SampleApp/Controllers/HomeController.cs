7526e62ede7c2b6394269e9c3a9a5c8bc71694e6
denis_levchenko <tranzi90@gmail.com>
2022-02-05 21:13:27 +0300

    deployment

---
+++ SampleApp/Controllers/HomeController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace SampleApp.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult List()
+        {
+            List<string> list = new List<string>();
+            list.Add("element 1");
+            list.Add("element 2");
+            list.Add("element 3");
+            list.Add("element 4");
+
+            return View(list);
+        }
+    }
+}
